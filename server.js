00530bc126b21998ff9d1f82364a1346f910ee21
harsh mungra <harshmungra255@gmail.com>
2024-11-25 12:21:46 +0530

    cors update

--- server.js
+++ server.js
@@ -12,8 +12,14 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(cors());
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    });
 
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGOATLAS_URI, {
     // useNewUrlParser: true,
     // useUnifiedTopology: true
 }).then(() => console.log('MongoDB connected...'))
